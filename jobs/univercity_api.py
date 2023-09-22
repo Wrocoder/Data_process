424fac6e90df6ce0d4c69efa1f4cac63fef7f054
polwroacc <polwroacc@gmail.com>
2023-09-22 13:11:48 +0200

    async api requests

--- jobs/univercity_api.py
+++ jobs/univercity_api.py
@@ -1,8 +1,9 @@
+import asyncio
 import os
 
+import httpx
 import pandas as pd
 
-from extraction.ApiScraper import APIScraper
 from helper.job_helper.decors import timing_and_size
 from helper.job_helper.job_helpers import get_config, to_csv_and_load, add_ts_col_to_df, get_countries
 from logger import LOGGER
@@ -13,19 +14,27 @@ pd.options.display.max_rows = 200
 pd.options.display.width = 2000
 
 
+async def fetch_data_from_api(url):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            return pd.DataFrame.from_dict(response.json())
+        except httpx.RequestError as e:
+            print(f"Request error occurred: {e}")
+        except httpx.HTTPError as e:
+            print(f"HTTP error occurred: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
 @timing_and_size
-def uni_main():
+async def uni_main():
     countries = get_countries()
     CONFIG = get_config()
-    df = pd.DataFrame()
-
-    for code, country in countries.items():
-        url = f"{CONFIG['universityData']['initial']['url']}{country}"
-        pipeline = APIScraper(url, CONFIG['universityData']['name'])
-        LOGGER.info(f'Processing {pipeline.__repr__()} data for {country}')
-        result_df = pipeline.run()
-        df = pd.concat([df, result_df], ignore_index=True)
-
+    main_url = CONFIG['universityData']['initial']['url']
+    as_resp = [fetch_data_from_api(f"{main_url}{country}") for code, country in countries.items()]
+    results = await asyncio.gather(*as_resp)
+    df = pd.concat([item for item in results], ignore_index=True)
     name = CONFIG['universityData']['name']
     home_path = f"{os.environ['HOME']}{CONFIG['universityData']['initial']['loadPath']}{name}/{name}.csv"
     LOGGER.info(f'Loading data to: {home_path}')
@@ -35,4 +44,4 @@ def uni_main():
 
 if __name__ == "__main__":
     LOGGER.info("Starting process")
-    uni_main()
+    asyncio.run(uni_main())
