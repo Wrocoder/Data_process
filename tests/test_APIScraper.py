4935f9bd3b49d57bf03c602b5b2a736ffa935d7f
polwroacc <polwroacc@gmail.com>
2023-09-22 10:03:08 +0200

    tests

---
+++ tests/test_APIScraper.py
@@ -0,0 +1,63 @@
+import unittest
+from unittest.mock import Mock, patch
+
+import pandas as pd
+import requests
+
+from extraction import ApiScraper
+
+
+class TestAPIScraper(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://example.com/api'
+        self.name = 'Test Scraper'
+        self.params = {'param1': 'value1', 'param2': 'value2'}
+        self.scraper = ApiScraper.APIScraper(self.url, self.name, self.params)
+
+    def test_init(self):
+        self.assertEqual(self.scraper.url, self.url)
+        self.assertEqual(self.scraper.name, self.name)
+        self.assertEqual(self.scraper.params, self.params)
+        self.assertIsNone(self.scraper.df)
+
+    @patch('requests.get')
+    def test_extract_via_api_successful(self, mock_requests_get):
+        mock_response = Mock()
+        mock_response.json.return_value = {'data': [1, 2, 3]}
+        mock_requests_get.return_value = mock_response
+
+        response = self.scraper.extract_via_api(params=self.params)
+
+        mock_requests_get.assert_called_once_with(self.url, params=self.params)
+        self.assertEqual(response.json(), {'data': [1, 2, 3]})
+
+    @patch('requests.get', side_effect=requests.exceptions.Timeout)
+    def test_extract_via_api_timeout_exception(self, mock_requests_get):
+        with self.assertRaises(SystemExit):
+            self.scraper.extract_via_api(params=self.params)
+
+    @patch('requests.get', side_effect=requests.exceptions.TooManyRedirects)
+    def test_extract_via_api_redirects_exception(self, mock_requests_get):
+        with self.assertRaises(SystemExit):
+            self.scraper.extract_via_api(params=self.params)
+
+    @patch('requests.get', side_effect=requests.exceptions.RequestException)
+    def test_extract_via_api_request_exception(self, mock_requests_get):
+        with self.assertRaises(SystemExit):
+            self.scraper.extract_via_api(params=self.params)
+
+    @patch('requests.get')
+    def test_run(self, mock_requests_get):
+        mock_response = Mock()
+        mock_response.json.return_value = {'data': [1, 2, 3]}
+        mock_requests_get.return_value = mock_response
+
+        result_df = self.scraper.run()
+
+        mock_requests_get.assert_called_once_with(self.url, params=self.params)
+        expected_df = pd.DataFrame({'data': [1, 2, 3]})
+        pd.testing.assert_frame_equal(result_df, expected_df)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_DataProcess.py
@@ -0,0 +1,61 @@
+import unittest
+from pyspark.sql import SparkSession
+from extraction.DataProcess import DataProcessor
+
+
+class TestDataProcessor(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize Spark session for testing
+        self.spark = SparkSession.builder \
+            .appName('SparkTestApp') \
+            .getOrCreate()
+        self.data_processor = DataProcessor()
+
+    def tearDown(self):
+        self.spark.stop()
+
+    def test_extract_data_csv(self):
+        csv_path = 'test_data.csv'
+        test_df = self.spark.createDataFrame([("John", 'john@mail.abc'), ("Alice", None)], ["name", "mail"])
+        test_df.write.mode('overwrite').option("header", True).csv(csv_path)
+        extracted_df = self.data_processor.extract_data('csv', path=csv_path)
+        self.assertTrue(test_df.collect() == extracted_df.collect())
+
+    def test_replace_null_values(self):
+        test_df = self.spark.createDataFrame([("John", None), (None, 25)], ["name", "age"])
+        replaced_df = self.data_processor.replace_null_values(test_df)
+        replaced_df.show()
+
+        # Check if null values are replaced correctly
+        self.assertFalse(replaced_df.collect()[0]["name"] is None)
+        self.assertFalse(replaced_df.collect()[1]["age"] is None)
+
+    def test_add_surrogate_key(self):
+        test_df = self.spark.createDataFrame([("John", 30), ("Alice", 25)], ["name", "age"])
+        df_with_key = self.data_processor.add_surrogate_key(test_df)
+        self.assertTrue("surrogate_key" in df_with_key.columns)
+
+    def test_transform_data(self):
+        test_df = self.spark.createDataFrame([("John", None), (None, 25)], ["name", "age"])
+        self.data_processor.df = test_df
+        transformed_df = self.data_processor.transform_data()
+
+        # Check if null values are replaced and a surrogate key column is added
+        self.assertFalse(transformed_df.collect()[0]["name"] is None)
+        self.assertFalse(transformed_df.collect()[1]["age"] is None)
+        self.assertTrue("surrogate_key" in transformed_df.columns)
+
+    def test_load_data(self):
+        test_df = self.spark.createDataFrame([("John", 30), ("Alice", 25)], ["name", "age"])
+        self.data_processor.df = test_df
+        destination = 'test_output.parquet'
+        part_cols = ['name']
+        self.data_processor.load_data(destination, part_cols)
+
+        # Verify if the destination path exists
+        self.assertTrue(self.data_processor.spark.read.parquet(destination).count() > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
